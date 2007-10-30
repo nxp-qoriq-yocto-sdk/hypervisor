439422fd2969fd89b2a9269cdd306522fe67ff82
scott.wood@freescale.com <scott.wood@freescale.com>
2007-10-30 15:11:19 -0500

    src/init.c whitespace cleanup
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: Stuart Yoder <stuart.yoder@freescale.com>

--- src/init.c
+++ src/init.c
@@ -1,47 +1,37 @@
-
 #include "uv.h"
 #include "console.h"
 
 extern void tlb1_init(void);
 
-static void  core_init(void);
+static void core_init(void);
 
 void init(unsigned long devtree_ptr)
 {
+	core_init();
+	console_init();
 
-    core_init();
-
-    console_init();
-
-    printh("=======================================\n");
-    printh("Freescale Ultravisor 0.1\n");
+	printf("=======================================\n");
+	printf("Freescale Ultravisor 0.1\n");
 
 #if 0
+	if (first instance) {
+		platform_hw_init();
+		uv_global_init();
+	}
 
-    if (first instance) {
-        platform_hw_init();
-        uv_global_init();
-    }
+	uv_instance_init();
 
-    uv_instance_init();
+	guest_init();
 
-    guest_init();
+	run_guest();
 
-    run_guest();
-
-    /* run_guest() never returns */
+	/* run_guest() never returns */
 
 #endif
-
 }
-
 
 static void core_init(void)
 {
-
-    /* set up a TLB entry for CCSR space */
-    tlb1_init();
-
+	/* set up a TLB entry for CCSR space */
+	tlb1_init();
 }
-
-
