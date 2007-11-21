49617b2be9ffb43716793607f344a2a4c65a4939
Scott Wood <scottwood@freescale.com>
2007-11-21 13:16:36 -0600

    Output DEAR, ESR, and a traceback when dumping registers.
    Signed-off-by: Scott Wood <scottwood@freescale.com>

--- src/trap.c
+++ src/trap.c
@@ -11,12 +11,30 @@ struct powerpc_exception {
 	char *name;
 };
 
+void traceback(trapframe_t *regs)
+{
+	unsigned long *sp = ((unsigned long *)regs->gpregs[1]);
+	printf("sp %p %lx %lx %lx %lx\n", sp, sp[0], sp[1], sp[2], sp[3]);
+	sp = ((unsigned long **)regs->gpregs[1])[0];
+
+	printf("Traceback: ");
+	
+	for (int i = 1; sp != NULL; i++, sp = (unsigned long *)sp[0]) {
+		if ((i & 7) == 0)
+			printf("\n");
+		
+		printf("0x%08lx ", sp[1] - 4);
+	}
+
+	printf("\n");
+}
+
 void dump_regs(trapframe_t *regs)
 {
-	printf("NIP 0x%08x MSR 0x%08x LR 0x%08x EXC %d\n"
-	       "CTR 0x%08x CR 0x%08x XER 0x%08x\n",
-	       regs->srr0, regs->srr1, regs->lr, regs->exc,
-	       regs->ctr, regs->cr, regs->xer);
+	printf("NIP 0x%08x MSR 0x%08x LR 0x%08x ESR 0x%08x EXC %d\n"
+	       "CTR 0x%08x CR 0x%08x XER 0x%08x DEAR 0x%08x\n",
+	       regs->srr0, regs->srr1, regs->lr, mfspr(SPR_ESR), regs->exc,
+	       regs->ctr, regs->cr, regs->xer, mfspr(SPR_DEAR));
 
 	for (int i = 0; i < 32; i++) {
 		printf("r%02d 0x%08x  ", i, regs->gpregs[i]);
@@ -24,6 +42,9 @@ void dump_regs(trapframe_t *regs)
 		if ((i & 3) == 3)
 			printf("\n");
 	}
+
+	if (!(regs->srr1 & MSR_GS))
+		traceback(regs);
 }
 
 static struct powerpc_exception powerpc_exceptions[] = {
