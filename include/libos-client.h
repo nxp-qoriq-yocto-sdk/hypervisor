da3c889e43323809db900b56e2dc36ec77ca4cae
Scott Wood <scottwood@freescale.com>
2007-12-07 18:03:25 -0600

    Add libos-client.h
    Signed-off-by: Scott Wood <scottwood@freescale.com>

---
+++ include/libos-client.h
@@ -0,0 +1,25 @@
+#ifndef LIBOS_CLIENT_H
+#define LIBOS_CLIENT_H
+
+#define PHYSBASE 0
+#define HYPERVISOR
+
+#ifndef _ASM
+typedef struct {
+	struct gcpu_t *gcpu;
+} client_cpu_t;
+#endif
+
+#define EXC_CRIT_INT_HANDLER critical_interrupt
+#define EXC_MCHECK_HANDLER powerpc_mchk_interrupt
+#define EXC_DSI_HANDLER data_storage
+#define EXC_ALIGN_HANDLER reflect_trap
+#define EXC_PROGRAM_HANDLER reflect_trap
+#define EXC_FPUNAVAIL_HANDLER reflect_trap
+#define EXC_DECR_HANDLER decrementer
+#define EXC_DTLB_HANDLER dtlb_miss
+#define EXC_GDOORBELL_HANDLER guest_doorbell
+#define EXC_HCALL_HANDLER hcall
+#define EXC_EHPRIV_HANDLER hvpriv
+
+#endif
