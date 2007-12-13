c220c20bd8b034d3fda6a8089d4e90ea1a3f6a14
Stuart Yoder <stuart.yoder@freescale.com>
2007-12-13 16:07:55 -0600

    CCSR base and UART addr is now passed to libos console init routine

--- src/init.c
+++ src/init.c
@@ -20,10 +20,16 @@ static void tlb1_init(void);
 static void core_init(void);
 void start_guest(void);
 
+/* hardcoded hack for now */
+#define CCSRBAR_PA              0xfe000000
+#define CCSRBAR_VA              0x01000000
+#define CCSRBAR_SIZE            TLB_TSIZE_16M
+#define UART_OFFSET		0x11c500
+
 void start(unsigned long devtree_ptr)
 {
 	core_init();
-	console_init();
+	console_init(CCSRBAR_VA + UART_OFFSET);
 
 	printf("=======================================\n");
 	printf("Freescale Ultravisor 0.1\n");
@@ -57,14 +63,13 @@ static void core_init(void)
 	tlb1_init();
 }
 
-/* hardcoded hack for now */
-#define CCSRBAR_PA              0xfe000000
-#define CCSRBAR_VA              0x01000000
-#define CCSRBAR_SIZE            TLB_TSIZE_16M
+extern int print_ok;  /* set to indicate printf can work now */
 
 static void tlb1_init(void)
 {
 
-        tlb1_set_entry(62, CCSRBAR_VA, CCSRBAR_PA, CCSRBAR_SIZE, TLB_MAS2_IO,
-                       TLB_MAS3_KERN, 0, 0, TLB_MAS8_HV);
+	tlb1_set_entry(62, CCSRBAR_VA, CCSRBAR_PA, CCSRBAR_SIZE, TLB_MAS2_IO,
+		TLB_MAS3_KERN, 0, 0, TLB_MAS8_HV);
+
+	print_ok = 1;
 }
