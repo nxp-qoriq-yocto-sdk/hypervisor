nxp-qoriq-yocto-sdk/hypervisor

commit 6f4a041304bd1a4df2b7f6ac24f01ce54935b93f

add hypervisor node to guest device trees
-add hypervisor node to guest dev trees as per
 the latest spec
-move the handles container under /hypervisor
-misc cleanup

test/gdb-test/gdb-p1.dts
@@ -71,10 +71,13 @@
 		};
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
+			interrupt-parent = <&vmpic>;
+		};
 	};
 
 	chosen {

test/hcalls-test/hcalls-p1.dts
@@ -71,19 +71,22 @@
 		};
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
-		
-		byte-channel0 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel1";
-		};
-
-		byte-channel1 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel2";
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
+			interrupt-parent = <&vmpic>;
+			
+			byte-channel0 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel1";
+			};
+	
+			byte-channel1 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel2";
+			};
 		};
 	};
 

test/hello-test/hello-p1.dts
@@ -71,19 +71,29 @@
 		};
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
-		
-		byte-channel0 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel1";
-		};
+	vmpic:vmpic {
+		compatible = "fsl,hv-vmpic";
+		interrupt-controller;
+		#interrupt-cells = <2>;
+		fsl,hv-phandle-ref = <&vmpic>;
+	};
 
-		byte-channel1 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel2";
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
+			interrupt-parent = <&vmpic>;
+			
+			byte-channel0 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel1";
+			};
+	
+			byte-channel1 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel2";
+			};
 		};
 	};
 

test/hello-test/hello.c
@@ -57,7 +57,7 @@ void start(void *devtree_ptr)
 
 	printf("Hello World\n");
 
-	node = fdt_path_offset(devtree_ptr, "/handles/byte-channel1");
+	node = fdt_path_offset(devtree_ptr, "/hypervisor/handles/byte-channel1");
 	if (node < 0) {
 		printf("0 device tree error %d\n",node);
 		return;

test/ipi_doorbell-test/ipi-p1.dts
@@ -72,30 +72,40 @@
 
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
+	vmpic:vmpic {
+		compatible = "fsl,hv-vmpic";
+		interrupt-controller;
+		#interrupt-cells = <2>;
+		fsl,hv-phandle-ref = <&vmpic>;
+	};
 
-		byte-channel0 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel1";
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
+			interrupt-parent = <&vmpic>;
+	
+			byte-channel0 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel1";
+			};
+	
+			byte-channel1 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel2";
+			};
+	
+			doorbell@0 {
+	               compatible = "fsl,doorbell-send";
+	               fsl,endpoint = "/doorbells/doorbell@0";
+	            };
+			doorbell@1 {
+	               compatible = "fsl,doorbell-receive";
+	               fsl,endpoint = "/doorbells/doorbell@0";
+	            };
+	
 		};
-
-		byte-channel1 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel2";
-		};
-
-		doorbell@0 {
-               compatible = "fsl,doorbell-send";
-               fsl,endpoint = "/doorbells/doorbell@0";
-            };
-		doorbell@1 {
-               compatible = "fsl,doorbell-receive";
-               fsl,endpoint = "/doorbells/doorbell@0";
-            };
-
 	};
 
 	chosen {

test/ipi_doorbell-test/ipi-p2.dts
@@ -75,28 +75,38 @@
 		};
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
+	vmpic:vmpic {
+		compatible = "fsl,hv-vmpic";
+		interrupt-controller;
+		#interrupt-cells = <2>;
+		fsl,hv-phandle-ref = <&vmpic>;
+	};
 
-		byte-channel@0 {
-			reg = <0>;
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "/uartmux";
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
+			interrupt-parent = <&vmpic>;
+	
+			byte-channel@0 {
+				reg = <0>;
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "/uartmux";
+			};
+	
+			gpio {
+				reg = <1>;
+				compatible = "fsl,hv-gpio";
+				fsl,real-dev = "gpio";
+				fsl,bitmask = <020033ff>;
+			};
+	
+			door-bell@0 {
+	               compatible = "fsl,doorbell-receive";
+	               fsl,endpoint = "/doorbells/doorbell@0";
+	            };
 		};
-
-		gpio {
-			reg = <1>;
-			compatible = "fsl,hv-gpio";
-			fsl,real-dev = "gpio";
-			fsl,bitmask = <020033ff>;
-		};
-
-		door-bell@0 {
-               compatible = "fsl,doorbell-receive";
-               fsl,endpoint = "/doorbells/doorbell@0";
-            };
 	};
 
 	chosen {

test/linux/linux-p1.dts
@@ -89,22 +89,24 @@
 		fsl,hv-phandle-ref = <&vmpic>;
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
-		
-		byte-channel@0 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel0";
-			interrupt-parent = <&vmpic>;
-		};
-
-		gpio {
-			reg = <1>;
-			compatible = "fsl,hv-gpio";
-			fsl,real-dev = "gpio";
-			fsl,bitmask = <103ecc00>;
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
+			
+			byte-channel@0 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel0";
+				interrupt-parent = <&vmpic>;
+			};
+	
+			gpio {
+				reg = <1>;
+				compatible = "fsl,hv-gpio";
+				fsl,real-dev = "gpio";
+				fsl,bitmask = <103ecc00>;
+			};
 		};
 	};
 
@@ -113,10 +115,5 @@
 		bootargs = "console=ttyS0,115200";
 		linux,initrd-start = <01300000>;
 		linux,initrd-end = <013ca000>;
-	};
-
-	aliases {
-//		stdout = "/handles/byte-channel@0";
-//		stdout = "/ccsr@fe000000/serial@11d500";
 	};
 };

test/linux/linux-p2.dts
@@ -105,39 +105,39 @@
 		fsl,hv-phandle-ref = <&vmpic>;
 	};
 
-	handles {
-		#address-cells = <1>;
-		#size-cells = <0>;
-		compatible = "fsl,hv-handles";
-		
-		byte-channel@0 {
-			compatible = "fsl,hv-byte-channel";
-			fsl,endpoint = "byte-channel1";
+	hypervisor {
+		handles {
+			#address-cells = <1>;
+			#size-cells = <0>;
+			compatible = "fsl,hv-handles";
 			interrupt-parent = <&vmpic>;
-		};
-
-		gpio {
-			reg = <1>;
-			compatible = "fsl,hv-gpio";
-			fsl,real-dev = "gpio";
-			fsl,bitmask = <020033ff>;
-		};
-
-		partition@0 {
-			compatible = "fsl,hv-partition";
-			fsl,endpoint = "/partitions/part1";
+			
+			byte-channel@0 {
+				compatible = "fsl,hv-byte-channel";
+				fsl,endpoint = "byte-channel1";
+				interrupt-parent = <&vmpic>;
+			};
+	
+			gpio {
+				reg = <1>;
+				compatible = "fsl,hv-gpio";
+				fsl,real-dev = "gpio";
+				fsl,bitmask = <020033ff>;
+			};
+	
+			partition@0 {
+				compatible = "fsl,hv-partition";
+				fsl,endpoint = "/partitions/part1";
+			};
 		};
 	};
 
 	chosen {
-//		linux,stdout-path = "/ccsr@fe000000/serial@11d600";
-//		bootargs = "console=ttyS0,115200";
 		linux,initrd-start = <01300000>;
 		linux,initrd-end = <013ca000>;
 	};
 	
 	aliases {
-		stdout = "/handles/byte-channel@0";
-//		stdout = "/ccsr@fe000000/serial@11d600";
+		stdout = "/hypervisor/handles/byte-channel@0";
 	};
 };

commit ef6ad9099f260432375ec7da718609fbaa0a0117

Add vmpic node to guest device trees.
This fixes a build break introduced by 6f4a0413
("add hypervisor node to guest device tree").

Signed-off-by: Scott Wood <scottwood@freescale.com>

test/gdb-test/gdb-p1.dts
@@ -71,6 +71,13 @@
 		};
 	};
 
+	vmpic:vmpic {
+		compatible = "fsl,hv-vmpic";
+		interrupt-controller;
+		#interrupt-cells = <2>;
+		fsl,hv-phandle-ref = <&vmpic>;
+	};
+
 	hypervisor {
 		handles {
 			#address-cells = <1>;

test/hcalls-test/hcalls-p1.dts
@@ -71,6 +71,13 @@
 		};
 	};
 
+	vmpic:vmpic {
+		compatible = "fsl,hv-vmpic";
+		interrupt-controller;
+		#interrupt-cells = <2>;
+		fsl,hv-phandle-ref = <&vmpic>;
+	};
+
 	hypervisor {
 		handles {
 			#address-cells = <1>;