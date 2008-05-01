dce3c79d47e1fc6b26232187001cf065b4f7463f
Stuart Yoder <stuart.yoder@freescale.com>
2008-04-30 22:26:55 -0500

    add new tests to Makefiles, run-all script
    Signed-off-by: Stuart Yoder <stuart.yoder@freescale.com>

--- test/run-all.sh
+++ test/run-all.sh
@@ -6,6 +6,8 @@ files="test/hello-test/hv-hello.simics \
 	test/linux/hv-linux-2p.simics \
 	test/ipi_doorbell-test/hv-ipi.simics \
 	test/ipi_doorbell-test/hv-ipi-2p.simics \
+	test/whoami-test/hv-whoami.simics \
+	test/hcalls-test/hv-hcalls.simics \
 	test/fit-test/hv-fit.simics"
 
 for i in $files
