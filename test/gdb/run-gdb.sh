87170a61a3609b53745ef7c85e3ee381f6c4669f
Stuart Yoder <stuart.yoder@freescale.com>
2008-08-15 13:38:55 -0500

    update gdb unit test
    -adding missing gdb scripts
    -use proper name of host gdb program
    
    Signed-off-by: Stuart Yoder <stuart.yoder@freescale.com>

--- test/gdb/run-gdb.sh
+++ test/gdb/run-gdb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ../../tools/mux_server/mux_server localhost:9124 9000 9001 9002 9003 9004 9005 9006 9007 &
-xterm -e "powerpc-unknown-linux-gnu-gdb-6.8.50.20080507 -x gdb0" &
-xterm -e "powerpc-unknown-linux-gnu-gdb-6.8.50.20080507 -x gdb1" &
-xterm -e "powerpc-unknown-linux-gnu-gdb-6.8.50.20080507 -x gdb2" &
-xterm -e "powerpc-unknown-linux-gnu-gdb-6.8.50.20080507 -x gdb3" &
+xterm -e "${CROSS_COMPILE}gdb -x gdb0" &
+xterm -e "${CROSS_COMPILE}gdb -x gdb1" &
+xterm -e "${CROSS_COMPILE}gdb -x gdb2" &
+xterm -e "${CROSS_COMPILE}gdb -x gdb3" &
