2966be3f228efcaac7229c69631f63c14742e44f
Varun Sethi <Varun.Sethi@freescale.com>
2009-01-28 16:26:37 +0530

    Fix default allocation for CPC ways.
    Transactions for a particular PMA should allocate in CPC, even if
    "allocate-cpc-ways" property is not specified for it.
    
    Also, we should not consider the already programmed values in the
    CPCP[IAW]Rn registers. CPCP[IAW]R0 registers value should be set
    to hardware reset values. This would ensure CPC to operate as a
    non partitioned cache (all ways shared across all PMAs).
    
    Signed-off-by: Varun Sethi <Varun.Sethi@freescale.com>
    Signed-off-by: Stuart Yoder <stuart.yoder@freescale.com>

--- src/cpc.c
+++ src/cpc.c
@@ -36,37 +36,36 @@
 
 static cpc_dev_t cpcs[NUMCPCS];
 
-static inline void set_cpc_reg_index(int num, int index)
+static inline void reserve_partition_reg(int cpc_index, int pir_num)
 {
-	cpcs[num].cpc_reg_map |= 1 << index;
+	cpcs[cpc_index].cpc_reg_map |= 1 << pir_num;
 }
 
-static inline void set_cpc_way_map(int num, uint32_t val)
+static inline void reserve_way(int cpc_index, uint32_t way)
 {
-	cpcs[num].cpc_way_map |= val >> count_lsb_zeroes(val);
+	cpcs[cpc_index].cpc_way_map |= way >> count_lsb_zeroes(way);
 }
 
-static void init_cpc_dev(int num, void *vaddr)
+static inline void remove_pir0_ways(int cpc_index, uint32_t way_mask)
 {
-	int i;
+	uint32_t val;
 
-	cpcs[num].cpc_part_base = (cpc_part_reg_t *)((uint32_t)vaddr + CPCPIR0);
+	val = in32(&cpcs[cpc_index].cpc_part_base[0].cpcpwr);
+	val &= ~way_mask;
+	out32(&cpcs[cpc_index].cpc_part_base[0].cpcpwr, val);
+}
+
+static void init_cpc_dev(int cpc_index, void *vaddr)
+{
+	cpcs[cpc_index].cpc_part_base = (cpc_part_reg_t *)((uint32_t)vaddr + CPCPIR0);
 	/*cpc_reg_map requires 16 bits*/
-	cpcs[num].cpc_reg_map = ~0xffffUL;
+	cpcs[cpc_index].cpc_reg_map = ~0xffffUL;
 
-	for (i = 0; i < NUM_PART_REGS; i++) {
-		uint32_t val;
+	out32(&cpcs[cpc_index].cpc_part_base[0].cpcpir, CPCPIR0_RESET_MASK);
+	out32(&cpcs[cpc_index].cpc_part_base[0].cpcpar, CPCPIR0_RESET_MASK);
+	out32(&cpcs[cpc_index].cpc_part_base[0].cpcpwr, CPCPIR0_RESET_MASK);
 
-		val = in32(&cpcs[num].cpc_part_base[i].cpcpir);
-		if (val == CPCPIR0_RESET_MASK && i == 0)
-			continue;
-
-		if (val) {
-			set_cpc_reg_index(num, i);
-			val = in32(&cpcs[num].cpc_part_base[i].cpcpwr);
-			set_cpc_way_map(num, val);
-		}
-	}
+	reserve_partition_reg(cpc_index, 0);
 }
 
 
@@ -86,7 +85,7 @@ static int cpc_probe(driver_t *drv, device_t *dev)
 	}
 
 	/* FIXME : It's assumed that CPC start addr would be
-	 * CCSRBASE + CPC[num] offset.
+	 * CCSRBASE + CPC[cpc_index] offset.
 	 */
 	if ((dev->regs[0].start & 0xfffff) == CPCADDR0)
 		init_cpc_dev(0, dev->regs[0].virt);
@@ -98,22 +97,22 @@ static int cpc_probe(driver_t *drv, device_t *dev)
 	return 0;
 }
 
-static inline int get_free_cpcpir(int num)
+static inline int get_free_cpcpir(int cpc_index)
 {
 
-	int val = count_lsb_zeroes(~cpcs[num].cpc_reg_map);
+	int val = count_lsb_zeroes(~cpcs[cpc_index].cpc_reg_map);
 
 	return val;
 }
 
-static cpc_part_reg_t *allocate_ways(int num, dt_prop_t *prop, uint32_t csdid)
+static cpc_part_reg_t *allocate_ways(int cpc_index, dt_prop_t *prop, uint32_t csdid)
 {
 	const uint32_t *ways;
 	uint32_t val = 0;
-	int numways, index, i;
+	int numways, pir_num, i;
 
-	index = get_free_cpcpir(num);
-	if (index == -1)
+	pir_num = get_free_cpcpir(cpc_index);
+	if (pir_num == -1)
 		return NULL;
 
 	numways = prop->len / sizeof(uint32_t);
@@ -125,7 +124,7 @@ static cpc_part_reg_t *allocate_ways(int num, dt_prop_t *prop, uint32_t csdid)
 					"Invalid CPC way number %d\n", ways[i]);
 			continue;
 		}
-		if (!(cpcs[num].cpc_way_map & (1 << ways[i])))
+		if (!(cpcs[cpc_index].cpc_way_map & (1 << ways[i])))
 			val |= (1 << (31 - ways[i]));
 		else
 			printlog(LOGTYPE_CPC, LOGLEVEL_ERROR,
@@ -135,15 +134,17 @@ static cpc_part_reg_t *allocate_ways(int num, dt_prop_t *prop, uint32_t csdid)
 	if (val == 0)
 		return NULL;
 
+	remove_pir0_ways(cpc_index, val);
+
 	/* CPCPIRn is a bitmap of CSDIDs */
-	out32(&cpcs[num].cpc_part_base[index].cpcpir, 1 << (31 - csdid));
-	set_cpc_reg_index(num, index);
+	out32(&cpcs[cpc_index].cpc_part_base[pir_num].cpcpir, 1 << (31 - csdid));
+	reserve_partition_reg(cpc_index, pir_num);
 
-	out32(&cpcs[num].cpc_part_base[index].cpcpar, CPCPAR_MASK);
-	out32(&cpcs[num].cpc_part_base[index].cpcpwr, val);
-	set_cpc_way_map(num, val);
+	out32(&cpcs[cpc_index].cpc_part_base[pir_num].cpcpar, CPCPAR_MASK);
+	out32(&cpcs[cpc_index].cpc_part_base[pir_num].cpcpwr, val);
+	reserve_way(cpc_index, val);
 
-	return &cpcs[num].cpc_part_base[index];
+	return &cpcs[cpc_index].cpc_part_base[pir_num];
 }
 
 void allocate_cpc_ways(dt_prop_t *prop, uint32_t tgt, uint32_t csdid, dt_node_t *node)
