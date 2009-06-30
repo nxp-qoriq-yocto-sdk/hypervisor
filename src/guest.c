nxp-qoriq-yocto-sdk/hypervisor

commit 1911f60cec2e0e46ff3df9270fa7b0a509dc1b95

map_guest_ranges: Don't parse ranges if we don't need to map or translate.
Also, don't let map-ranges inherit to non-directly-assigned nodes.

Signed-off-by: Scott Wood <scottwood@freescale.com>

src/guest.c
@@ -213,10 +213,21 @@ static int map_guest_ranges(dev_owner_t *owner)
 	const uint32_t *ranges;
 	uint32_t *newranges, *newranges_base;
 	dt_node_t *hwnode = owner->hwnode;
+	int map = 0;
 	int ret;
 
 	prop = dt_get_prop(hwnode, "ranges", 0);
 	if (!prop)
+		return 0;
+
+	if (dt_get_prop(owner->cfgnode, "map-ranges", 0) &&
+	    owner->direct == owner)
+		map = 1;
+
+	/* Don't parse the ranges at all if it's a non-reparented child
+	 * without map-ranges.
+	 */
+	if (!map && owner->gnode->parent->parent)
 		return 0;
 
 	dt_node_t *parent = hwnode->parent;
@@ -264,7 +275,6 @@ static int map_guest_ranges(dev_owner_t *owner)
 		assert(!addrbuf[0] && !addrbuf[1]);
 		addr = ((uint64_t)addrbuf[2] << 32) | addrbuf[3];
 
-		if (dt_get_prop(owner->cfgnode, "map-ranges", 0))
 			map_guest_addr_range(owner->guest, addr, addr, size);
 
 		memcpy(newranges, ranges, caddr * 4);

commit 44f0b20839379afee8df7ab3a19739385bed6c16

map_guest_ranges: Obey absence of map-ranges property in reparented nodes.
This was broken in commit 1911f60cec2e0e46ff3df9270fa7b0a509dc1b95.

Signed-off-by: Scott Wood <scottwood@freescale.com>

src/guest.c
@@ -345,7 +345,8 @@ static int map_guest_ranges(dev_owner_t *owner)
 		assert(!addrbuf[0] && !addrbuf[1]);
 		addr = ((uint64_t)addrbuf[2] << 32) | addrbuf[3];
 
-		map_dev_range(owner->guest, addr, size);
+		if (map_ranges)
+			map_dev_range(owner->guest, addr, size);
 
 		memcpy(newranges, ranges, caddr * 4);
 		newranges += caddr;