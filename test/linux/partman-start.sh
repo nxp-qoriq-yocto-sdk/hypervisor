a7e5c36d5664b057d292af44ed611f69900e6e08
Stuart Yoder <stuart.yoder@freescale.com>
2009-07-16 11:20:51 -0400

    add start scripts for partman scenarios to repo
    Signed-off-by: Stuart Yoder <stuart.yoder@freescale.com>

---
+++ test/linux/partman-start.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2009 Freescale Semiconductor, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+#  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
+#  NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+#  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+partman load -h p2-linux -f vmlinux -a 0x0
+partman load -h p2-linux -f rootfs.ext2.gz -a 0x1300000
+partman start -h p2-linux -e 0x0
+partman load -h p3-linux -f vmlinux -a 0x0
+partman load -h p3-linux -f rootfs.ext2.gz -a 0x1300000
+partman start -h p3-linux -e 0x0
