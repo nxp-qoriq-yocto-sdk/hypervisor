nxp-qoriq-yocto-sdk/hypervisor

commit 599d68f23e3a03f40737aacb44dae96e8c0d64b7

Don't try to start a partition whose initialization has failed.
Signed-off-by: Scott Wood <scottwood@freescale.com>

src/guest.c
@@ -1581,7 +1581,7 @@ static int partition_config(guest_t *guest)
 	return 0;
 }
 
-static void get_cpulist(guest_t *guest)
+static int get_cpulist(guest_t *guest)
 {
 	dt_node_t *node = guest->partition;
 	dt_prop_t *prop;
@@ -1593,7 +1593,7 @@ static void get_cpulist(guest_t *guest)
 			         "%s: bad cpus property in %s\n",
 			         __func__, node->name);
 
-			return;
+			return ERR_BADTREE;
 		}
 
 		guest->cpulist = malloc(prop->len);
@@ -1609,11 +1609,12 @@ static void get_cpulist(guest_t *guest)
 		         "%s: No cpus property in guest %s\n",
 		         __func__, node->name);
 
-	return;
+	return 0;
 
 nomem:
 	printlog(LOGTYPE_PARTITION, LOGLEVEL_ERROR,
 	         "%s: out of memory\n", __func__);
+	return ERR_NOMEM;
 }
 
 static uint32_t start_guest_lock;
@@ -1690,11 +1691,13 @@ guest_t *node_to_partition(dt_node_t *partition)
 			guests[i].privileged = 1;
 
 		guests[i].name = name;
-		guests[i].state = guest_starting;
+		guests[i].state = guest_starting_uninit;
 		guests[i].partition = partition;
 		guests[i].lpid = ++last_lpid;
 
-		get_cpulist(&guests[i]);
+		ret = get_cpulist(&guests[i]);
+		if (ret < 0)
+			return NULL;
 
 #ifdef CONFIG_DEBUG_STUB
 		init_stubops(&guests[i]);
@@ -3000,6 +3003,9 @@ static void start_partitions(void)
 
 	for (i = 0; i < last_lpid; i++) {
 		guest_t *guest = &guests[i];
+		if (guest->state != guest_starting)
+			continue;
+
 		gcpu_t *gcpu = guest->gcpus[0];
 
 		/* Add it anyway, as for no-auto this will be decreased

include/percpu.h
@@ -72,6 +72,10 @@ typedef enum {
 	guest_resuming = 6,
 
 	/* Internal substates */
+	guest_starting_min,
+	guest_starting_uninit,
+	guest_starting_max,
+
 	guest_stopping_min,
 	guest_stopping_percpu,
 	guest_stopping_max,

src/hcalls.c
@@ -293,6 +293,10 @@ static void hcall_partition_get_status(trapframe_t *regs)
 	regs->gpregs[3] = 0;  
 
 	/* Don't let internal states be public API. */
+	if (guest->state >= guest_starting_min &&
+	    guest->state <= guest_starting_max)
+		regs->gpregs[4] = guest_starting;
+
 	if (guest->state >= guest_stopping_min &&
 	    guest->state <= guest_stopping_max)
 		regs->gpregs[4] = guest_stopping;

commit e5cf6f6ee8d45d89d5e43922e2676bfe23760a19

node_to_partition: fix missing unlock on error path
Commit 599d68f23e3a03f40737aacb44dae96e8c0d64b7 introduced a new
error return from node_to_partition, but forgot to release the lock.

Switch to a shared error cleanup goto.

Signed-off-by: Scott Wood <scottwood@freescale.com>

src/guest.c
@@ -1645,10 +1645,9 @@ guest_t *node_to_partition(dt_node_t *partition)
 
 	if (i == last_lpid) {
 		if (last_lpid >= MAX_PARTITIONS) {
-			spin_unlock_intsave(&start_guest_lock, saved);
 			printlog(LOGTYPE_PARTITION, LOGLEVEL_ERROR,
 			         "node_to_partition: too many partitions\n");
-			return NULL;
+			goto err;
 		}
 
 		// We create the special doorbells here, instead of in
@@ -1656,20 +1655,17 @@ guest_t *node_to_partition(dt_node_t *partition)
 		// doorbells will be created for all partitions before the
 		// managers start looking for their managed partitions.
 		ret = create_guest_special_doorbells(&guests[i]);
-		if (ret < 0) {
-			spin_unlock_intsave(&start_guest_lock, saved);
-			return NULL;
-		}
+		if (ret < 0)
+			goto err;
 
 		name = dt_get_prop_string(partition, "label");
 		if (!name) {
 			/* If no label, use the partition node path. */
 			name = malloc(MAX_DT_PATH);
 			if (!name) {
-				spin_unlock_intsave(&start_guest_lock, saved);
 				printlog(LOGTYPE_PARTITION, LOGLEVEL_ERROR,
 				         "node_to_partition: out of memory\n");
-				return NULL;
+				goto err;
 			}
 
 			ret = dt_get_path(NULL, partition, name, MAX_DT_PATH);
@@ -1715,7 +1711,7 @@ guest_t *node_to_partition(dt_node_t *partition)
 
 		ret = get_cpulist(&guests[i]);
 		if (ret < 0)
-			return NULL;
+			goto err;
 
 #ifdef CONFIG_DEBUG_STUB
 		init_stubops(&guests[i]);
@@ -1725,6 +1721,10 @@ guest_t *node_to_partition(dt_node_t *partition)
 
 	spin_unlock_intsave(&start_guest_lock, saved);
 	return &guests[i];
+
+err:
+	spin_unlock_intsave(&start_guest_lock, saved);
+	return NULL;
 }
 
 static void guest_core_init(guest_t *guest)