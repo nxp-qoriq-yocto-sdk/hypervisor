81bf43ecf7a1f528b1744f23f1ed17b6faf3d866
Tudor Laurentiu <laurentiu.tudor@freescale.com>
2012-09-07 15:34:49 +0000

    test: add p5040ds support in unit tests
    Signed-off-by: Laurentiu Tudor <Laurentiu.Tudor@freescale.com>

---
+++ test/linux/partman-start_p5040ds.sh
@@ -0,0 +1,35 @@
+#
+# Copyright 2012 Freescale Semiconductor, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+#  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
+#  NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+#  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+rootfs=rootfs.ext2.gz.uboot
+
+if [ $# -gt 0 ]; then
+	rootfs=$1
+fi
+
+partman load -h p2-linux -f vmlinux -a 0x0
+partman load -h p2-linux -f $rootfs -a 0x1300000 -r
+partman start -h p2-linux -e 0x0
+partman load -h p3-linux -f vmlinux -a 0x0
+partman load -h p3-linux -f $rootfs -a 0x1300000 -r
+partman start -h p3-linux -e 0x0
